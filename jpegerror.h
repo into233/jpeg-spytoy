1835da2dc2d00d6a286c18eb2d9684c17b721862
into233 <slomovbus@sina.com>
2019-11-04 17:48:27 +0800

    delete unessary file and add gitignore, fix bugs

--- jpegerror.h
+++ jpegerror.h
@@ -2,10 +2,10 @@
 #define FILE_FD_ERR     				-1		//无效的文件描述符
 #define FILE_READ_ERR   				-2		//读文件失败
 #define FILE_WRITE_ERR  				-3		//写文件失败
-#define INVALID_METAFILE_ERR			-4		//无效的种子文件
+#define INVALID_METAFILE_ERR			-4		//无效的文件
 #define INVALID_HUFFMAN_CODE_ERROR      -5
 #define INVALID_SWITCH_ERROR            -6
-
+#define UNKNOWN_ERROR                   -7
 #define INVALID_PARAMETER_ERR			-10		//无效的函数参数
 #define INVALID_ALLOCATE_MEM_ERR		-11		//申请动态内存失败
 #define NO_BUFFER_ERR					-12		//没有足够的缓冲区
